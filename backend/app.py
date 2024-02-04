888c66d8c3e533da0cebee0055a7de7fcf0134c6
Mikolaj Kopec <mikolajkopec772@gmail.com>
2024-02-04 19:11:08 +0100

    create compression function

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "dupaa"
+
+def compress_image(file_stream, compressed_image_format="webp"):
+    image_to_compress = Image.open(file_stream)
+    compressed_image = image_to_compress.convert(
+        "P", palette=Image.ADAPTIVE, colors=256
+    )
+    img_bytes_arr = io.BytesIO()
+    compressed_image.save(img_bytes_arr, format=compressed_image_format)
+    img_bytes_arr.seek(0)
+    return img_bytes_arr
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        img = request.files.get("image_to_compress")
+        img_extension = "webp"
+        compressed_image_stream = compress_image(img, img_extension)
+        return send_file(
+            compressed_image_stream,
+            as_attachment=True,
+            download_name=f"compressed_image.{img_extension}",
+            mimetype="image/png",
+        )
+    return render_template("index.html", page_name="Strona główna")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
