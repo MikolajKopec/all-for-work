a8754749654c0e070cf408ac9044b7f197d95654
Mikolaj Kopec <mikolajkopec772@gmail.com>
2024-02-04 23:18:55 +0100

    separate img optimizer logic to standalone service

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,38 @@
 from flask import Flask, render_template, request, send_file
-from PIL import Image
-import io
+from .services.optimizer.img_optimizer import ImageOptimizer, ImageFormat
+from PIL import UnidentifiedImageError
+
 
 app = Flask(__name__)
-
-
-def compress_image(file_stream, compressed_image_format="webp"):
-    image_to_compress = Image.open(file_stream)
-    compressed_image = image_to_compress.convert(
-        "P", palette=Image.ADAPTIVE, colors=256
-    )
-    img_bytes_arr = io.BytesIO()
-    compressed_image.save(img_bytes_arr, format=compressed_image_format)
-    img_bytes_arr.seek(0)
-    return img_bytes_arr
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        img = request.files.get("image_to_compress")
-        img_extension = "webp"
-        compressed_image_stream = compress_image(img, img_extension)
-        return send_file(
-            compressed_image_stream,
-            as_attachment=True,
-            download_name=f"compressed_image.{img_extension}",
-            mimetype="image/png",
-        )
-    return render_template("index.html", page_name="Strona główna")
+        form_images = request.files.getlist("images_to_compress")
+        expected_extension = request.form.get("images_format").lower()
+        try:
+            print(form_images)
+            if len(form_images) == 1:
+                result = ImageOptimizer.compress_image(
+                    form_images[0], expected_extension
+                )
+                download_name = f"compressed_image.{expected_extension}"
+            else:
+                result = ImageOptimizer.compress_images(form_images, expected_extension)
+                download_name = "compressed_image.zip"
+            return send_file(
+                result,
+                as_attachment=True,
+                download_name=download_name,
+                mimetype="application/zip",
+            )
+        except UnidentifiedImageError:
+            print("No file")
+
+    return render_template(
+        "index.html", page_name="Home", image_formats=list(ImageFormat)
+    )
 
 
 if __name__ == "__main__":
